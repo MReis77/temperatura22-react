4fadf373edfed7ae9f97f1b4407f73b5709d5406
Marco <marcojunior.reis@hotmail.com>
2021-08-03 18:39:09 -0300

    first commit

---
+++ src/Calc.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import App from './App';
+import Fahrenheit from './Fahrenheit';
+import Celsius from './Celsius';
+
+
+
+class Calc extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+             temperature: 0,
+             scale: 'celsius'
+        }
+
+        this.updateCelsius = this.updateCelsius.bind(this);
+        this.updateFahrenheit = this.updateFahrenheit.bind(this);
+        this.temperatureInCelsius = this.temperatureInCelsius.bind(this);
+        this.temperatureInFahrenheit = this.temperatureInFahrenheit.bind(this);
+
+    }
+
+    temperatureInCelsius(){
+
+        if(this.state.scale === 'celsius')
+            return this.state.temperature;
+        return 
+    }
+                
+    temperatureInFahrenheit(){
+        if(this.state.scale === 'fahrenheit')
+            return this.state.temperature;
+        return    
+    }
+
+    updateCelsius(celsius){}
+
+    updateFahrenheit(celsius){}
+
+
+    render() {
+        return (  
+
+        
+        <div id="master">
+
+            <header id="topo">
+                < App />
+            </header >
+    
+            <section id="calc">
+
+                <div id="cels">
+                    < Celsius />
+                </div>
+
+                <div id="fah">
+                    < Fahrenheit />
+                </div>
+            </section>
+
+        </div>  
+        );
+    }
+
+
+
+
+}
+
+export default Calc;
